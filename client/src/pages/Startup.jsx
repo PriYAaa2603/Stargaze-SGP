cafdc99662b94bc7c5db0db0b113a49bb2bfab99
SarangaSiriwardhana9 <lasindusaranga99@gmail.com>
2024-05-06 19:31:12 +0530

    update startup page ui

--- client/src/pages/Startup.jsx
+++ client/src/pages/Startup.jsx
@@ -1,6 +1,7 @@
+/* eslint-disable no-unused-vars */
 import React from 'react';
-import BannerImg from '/banner.svg'; // Assuming banner.svg is in the same directory
-import back11 from '/bg3.jpg'; // Assuming bg3.jpg is in the same directory
+import BannerImg from '/banner.svg'; 
+import back11 from '/blackbg.jpg'; 
 import { Link } from 'react-router-dom';
 
 export default function Startup() {
@@ -8,14 +9,14 @@ export default function Startup() {
     <div className="flex flex-col items-center justify-center min-h-screen bg-cover bg-center" style={{ backgroundImage: `url(${back11})` }}>
       <div className="max-w-3xl md:mt-32 py-4 lg:mt-16 gap-y-36 mx-auto rounded-3xl text-center ">
         
-        <div className="bg-clip-padding  rounded-3xl p-4 backdrop-filter backdrop-blur-md bg-opacity-0">
+        <div className="">
         <div className='mb-4'>
-        <h1 className="text-5xl font-bold text-blue-400">Welcome to <span className='text-blue-100'>StarGaze</span></h1>
+        <h1 className="text-5xl font-bold text-blue-100">Welcome to <span className='text-blue-400'>StarGaze⚡</span></h1>
         <p className="text-lg text-white  mt-4 md:mt-4 px-8">Explore the wonders of the universe with daily astronomy news, breathtaking images, and insightful articles.</p>
         <div className=" ">
           <img src={BannerImg} alt="Banner" className="w-80 h-80 max-w-full mx-auto" />
         </div>
-        <Link to="/sign-up">
+        <Link to="/sign-in">
         <span className="mt-8 md:mt-8 mb-2  px-6 py-3 bg-blue-100 text-blue-900 text-base  font-extrabold rounded-lg shadow-md hover:bg-blue-200 focus:outline-none">Start Here</span>
         </Link>
         </div>
