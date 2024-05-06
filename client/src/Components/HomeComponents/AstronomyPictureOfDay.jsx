340e90a7a28ab8cd167afff2f014eade3a90992f
SarangaSiriwardhana9 <lasindusaranga99@gmail.com>
2024-05-06 20:50:41 +0530

    Create Astronomy area

--- client/src/Components/HomeComponents/AstronomyPictureOfDay.jsx
+++ client/src/Components/HomeComponents/AstronomyPictureOfDay.jsx
@@ -1,4 +1,6 @@
-import React, { useState, useEffect } from 'react';
+/* eslint-disable react/prop-types */
+import { useState, useEffect } from 'react';
+import back1 from '/blackbg.jpg';
 
 const AstronomyPictureOfDay = ({ apiKey }) => {
   const [apodData, setApodData] = useState(null);
@@ -25,12 +27,23 @@ const AstronomyPictureOfDay = ({ apiKey }) => {
   }
 
   return (
-    <div className="max-w-3xl bg-slate-50 p-10 mx-auto mt-8">
-      <h2 className="text-2xl font-bold mb-4">Astronomy Picture of the Day</h2>
-      <img src={apodData.url} alt={apodData.title} className="w-full h-auto rounded-lg shadow-md" />
-      <div className="mt-4">
-        <h3 className="text-lg font-semibold">{apodData.title}</h3>
-        <p className="mt-2 text-gray-600">{apodData.explanation}</p>
+    <div className="bg-cover bg-center px-8" style={{ backgroundImage: `url(${back1})` }} id='astronomy'>
+      <h1 className="text-4xl font-bold text-center text-gray-100 mb-4">Astronomy Picture of the Day 🔭
+</h1>
+      {/* sub title */}
+      <p className="text-center text-lg text-gray-300 mb-8">Discover the cosmos with the Astronomy Picture of the Day, showcasing the vast beauty of our universe.</p>
+      
+      <div className="max-w-7xl mx-auto py-10">
+        <div className="flex flex-col lg:flex-row lg:space-x-8 items-center">
+          <div className="lg:w-1/2">
+            
+            <h2 className="text-2xl font-semibold text-gray-300">{apodData.title}</h2>
+            <p className="text-gray-400 mt-4">{apodData.explanation}</p>
+          </div>
+          <div className="lg:w-1/2 mt-6 lg:mt-0">
+            <img src={apodData.url} alt={apodData.title} className="w-full h-auto rounded-md shadow-md" />
+          </div>
+        </div>
       </div>
     </div>
   );
